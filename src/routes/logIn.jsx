bf3d2d1ff6575639821993074f6f1976f11efeb4
Semilore <semiloremusibau@gmail.com>
2025-03-09 00:48:11 +0100

    google auth implemented

--- src/routes/logIn.jsx
+++ src/routes/logIn.jsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { auth } from '../utilities/firebase-config/config'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import { auth , googleProvider } from '../utilities/firebase-config/config'
+import { signInWithEmailAndPassword , signInWithPopup } from 'firebase/auth'
 import { useState } from 'react'
 import { useNavigate } from '@tanstack/react-router'
 
@@ -19,12 +19,22 @@ function logIn() {
     }
   }
 
+  const signInUserWithGoogle = () => {
+    try {
+      signInWithPopup(auth , googleProvider)
+      navigate({to : '/cartPage'})
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   return (
   <section className='login-section'>
     <div className='login-wrapper'>
       <input className='login-inputs' type="email" placeholder='Email...' onChange={(e) => setEmail(e.target.value)}/>
       <input className='login-inputs' type="password" placeholder='Password...' onChange={(e) => setPassword(e.target.value)}/>
       <button onClick={() => signInUser()} className='log-in-account'>Log In</button>
+      <button onClick={() => signInUserWithGoogle()} className='log-in-account'>Sign In With Google <i className="bi bi-google" ></i> </button>
     </div>
   </section>
   )
--- src/utilities/firebase-config/config.jsx
+++ src/utilities/firebase-config/config.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAXFM_1Ed32NKaDaJm15kpAUdVHQIM8nro",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
